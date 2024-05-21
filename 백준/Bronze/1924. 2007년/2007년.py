a441675f1d8bba6d17ee7f077ecf58daa872d820
Choi JaeHun <32566767+ash-hun@users.noreply.github.com>
2024-05-22 01:01:46 +0900

    [Bronze I] Title: 2007년, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,31 +1,11 @@
-x, y = map(int, input().split())
-
-big_month = [1, 3, 5, 7, 8, 10, 12]
-normal_month = [4, 6, 9, 11]
-small_month = [2]
-
 day = 0
-for i in range(1, x):
-    if i in big_month:
-        day += 31
-    elif i in normal_month:
-        day += 30
-    elif i in small_month:
-        day += 28
-
-day += y
-
-if day % 7 == 1:
-    print("MON")
-elif day % 7 == 2:
-    print("TUE")
-elif day % 7 == 3:
-    print("WED")
-elif day % 7 == 4:
-    print("THU")
-elif day % 7 == 5:
-    print("FRI")
-elif day % 7 == 6:
-    print("SAT")
-elif day % 7 == 0:
-    print("SUN")
+day_for_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day_of_week = ["SUN", "MON","TUE", "WED", "THU", "FRI", "SAT"]
+ 
+x, y = map(int,input().split())
+ 
+for i in range(x-1):
+    day = day + day_for_month[i]
+day = (day + y) % 7
+ 
+print(day_of_week[day])
